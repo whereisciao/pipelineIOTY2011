d79160ee94e252adb5219dc126a43897b7e3b51f
Steven Chau <steven.chau@mac.com>
2010-11-23 18:26:47 -0600

    New Template for full-width page.

---
+++ page_full_width.php
@@ -0,0 +1,27 @@
+<?php 
+/**
+ * Template Name: Page - Full Width
+ *
+ */
+
+get_header(); ?>
+
+<div class="container">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div class="post span-24 first last" id="post-<?php the_ID(); ?>">
+
+			<div class="entry">
+
+				<?php the_content(); ?>
+
+				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+
+			</div>
+
+			<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+		</div>
+
+		<?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
