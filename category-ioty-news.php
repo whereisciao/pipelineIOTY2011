9490b071d5ac5fe8eb2781625fbda5e0ccf8e4ae
Steven Chau <steven.chau@mac.com>
2011-12-05 15:53:30 -0800

    Set Feature Image on IOTY News.

--- category-ioty-news.php
+++ category-ioty-news.php
@@ -5,7 +5,7 @@
       <div class="container">
       	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       		<div class="post span-24 first last" id="post-<?php the_ID(); ?>">
-        		
+
 
       			<div class="entry span-14 first">
 
@@ -17,14 +17,26 @@
 
       			</div>
       			<div class="featured-image span-8 last">
-      			  <?php the_post_thumbnail(array(300, 200)); ?>              
+      			  <?php
+      			    $feature_image_url = get_post_meta($post->ID, 'feature_image_url', true);
+      			    if($feature_image_url){
+      			      ?>
+          			  <a href="<?php echo $feature_image_url?>"><?php the_post_thumbnail(); ?></a>
+      			      <?php
+      			    } else {
+      			      ?>
+          			  <?php the_post_thumbnail(); ?>
+      			      <?php
+      			    }
+      			  ?>
+
     			  </div>
       		</div>
 
       		<?php endwhile; endif; ?>
 
       </div>
-			
+
 	<?php else : ?>
 
 		<h2>Nothing found</h2>
